9752ab52be06f646a1c4bf9fee3227e91b10616e
tess-ahh <tessamariya295@gmail.com>
2026-06-04 12:23:29 +0530

    feat: add incidents API endpoint

--- ai_engine/api/app.py
+++ ai_engine/api/app.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 
+from ai_engine.evidence.incident_query import (
+    IncidentQuery
+)
+
 app = FastAPI(
     title="Campus Guardian AI",
     version="1.0.0"
 )
+
+query = IncidentQuery()
 
 
 @app.get("/")
@@ -11,3 +17,8 @@ def home():
     return {
         "message": "Campus Guardian AI API Running"
     }
+
+
+@app.get("/incidents")
+def get_incidents():
+    return query.load_incidents()
